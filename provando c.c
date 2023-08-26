6f078f3009914fa0207ec494ba58fd4030e4bbb0
braianhernan170 <braiancj17@gmail.com>
2023-08-26 15:31:37 -0300

    https://github.com/unlu-edu-ar/prog1-2023-hw-0-braianhernan170

---
+++ provando c.c
@@ -0,0 +1,54 @@
+
+
+
+
+// #include <stdio.h>
+
+// #include <stdlib.h>
+
+// #include <string.h>
+
+// int suma (int a , int b){
+//     int r ;
+//     r = a + b;
+//     return r;
+// }
+
+// int main (){
+//     int x1, x2,res;
+//     printf("ingrese un numero para sumar");
+//     printf("ingrese otro numero");
+//     res = suma (x1,x2);
+//     printf("%i", res);
+//     printf("\n");
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int a, b, c;
+//     scanf(" %d %d %d ", &a, &b, &c);
+//     int s = a + b + c;
+//     int p = a * b * c;
+//     float avg = (float)s / 3;
+//     printf(" %d
+// %d
+// %.3f ", s, p, avg);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
